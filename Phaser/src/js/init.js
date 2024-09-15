f3441638256562be8eb40574003a300be39c152c
201813880 <143803412+KaissoGithub@users.noreply.github.com>
2024-09-15 18:36:48 -0400

    Delete Phaser directory

--- Phaser/src/js/init.js
+++
@@ -1,9 +0,0 @@
-const config = {
-    type: Phaser.AUTO,
-    parent: "canvas-wrapper",
-    width: 1400,
-    height: 800,
-    scene: [Accueil, CommentJouer, Jeu, Victoire, Defaite, Credit],
-    transparent:true
-};
-const game = new Phaser.Game(config);
